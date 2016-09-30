628727bee44d87b2cc232c88b2032ef77b94da9a
Sergey Tkachenko <zevsariy.app@gmail.com>
2016-09-30 10:33:50 +0300

    Добавил холивары, теперь можно хетить Ap...

--- Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Controllers/MessagesController.cs
@@ -87,24 +87,6 @@ namespace Bot_Application1
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-        public string get_bill_phrase()
-        {
-            string[] phrases = new string[11] { "Своими успехами в бизнесе я обязан прежде всего способности сосредоточиться на перспективных целях, не поддаваясь соблазну решать сиюминутные задачи.",
-                "Миру наплевать на твое самоощущение и самоуважение. Мир ожидает от тебя каких-нибудь достижений, перед тем как принять во внимание твое чувство собственного достоинства.",
-                "Чудесно праздновать свой успех, но более важным является умение выносить уроки из своих провалов.",
-                "На прошлой неделе я вышел пообедать. Какой-то парень остановил меня и попросил денег. Я не знал, что делать. Потом он сказал, что я должен побывать на его домашней странице, и дал мне свой URL(унифицированный указатель ресурса). Я не понял, правду он говорит или нет, но строка оказалась настолько хороша, что я дал ему пять долларов. Так что он, возможно, бездомный человек с домашней страницей в Интернете.",
-                "У моих детей, конечно, будет компьютер. Но первым делом они получат книги.",
-                "Мы объясняем людям, что, если никто не смеялся хотя бы над одной из их идей, они, возможно, недостаточно творчески подходят к работе.",
-                "Я всегда буду искать ленивого человека для работы, ведь он найдет много легких путей для решения поставленной задачи.",
-                "Когда вам в голову пришла хорошая идея, действуйте незамедлительно.",
-                "Жизнь становится намного веселее, если подходить ко всем её вызовам творчески.",
-                "Чем я всегда рад поделиться, так это своим энтузиазмом.",
-                "Не люблю конференции во всяких экзотических местах. Такое ощущение, что чем приятнее место, тем меньше делается работы."};
-            Random rnd = new Random();
-            int ph_id = rnd.Next(0, 11);
-            return phrases[ph_id];
-        }
-
         public string great_cuter(string phrase)
         {
             phrase = phrase.ToLower();
@@ -114,12 +96,18 @@ namespace Bot_Application1
             phrase = phrase.Replace("что это ", "");
             phrase = phrase.Replace("определение ", "");
             phrase = phrase.Replace("?", "");
+            phrase = phrase.Replace("кто такой ", "");
+            phrase = phrase.Replace(" кто такой", "");
+            phrase = phrase.Replace("кто это ", "");
+            phrase = phrase.Replace(" кто это", "");
+            phrase = phrase.Replace("кто такая ", "");
+            phrase = phrase.Replace(" кто такая", "");
             return phrase;
         }
 
         public string get_answer(string query)
         {
-            string answer = "Пусто";
+            string answer = "Для получения справки по общению со мной скажите \"помощь\")";
             DBConnect MyDB = new DBConnect();
             if (MyDB.OpenConnection() == true)
             {
@@ -154,19 +142,71 @@ namespace Bot_Application1
                 return answer;
             }
         }
+
+        public string get_holywar_answer(string query)
+        {
+            string answer = "-1";
+            DBConnect MyDB = new DBConnect();
+            if (MyDB.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, MyDB.connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                bool flag = true;
+                while (dataReader.Read() && flag == true)
+                {
+                    flag = false;
+                    Random rnd = new Random();
+                    string s = dataReader["answer"].ToString();
+                    string[] split = s.Split(";".ToCharArray());
+                    int num = rnd.Next(0, split.Count());
+                    answer = split[num];
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                MyDB.CloseConnection();
+
+                //return list to be displayed
+                return answer;
+            }
+            else
+            {
+                return answer;
+            }
+        }
+
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
+                string holywar_text = activity.Text;
+                holywar_text = holywar_text.ToLower();
+                holywar_text = great_cuter(holywar_text);
+                holywar_text = holywar_text.Replace(" ", "%' OR question LIKE '%");
+
                 activity.Text = activity.Text.ToLower();
                 activity.Text = great_cuter(activity.Text);
                 activity.Text = activity.Text.Replace(" ", "%' AND question LIKE '%");
-                Console.WriteLine(activity.Text);
+                
                 string texta = "Пустой Текста";
-                texta = get_answer("Select answer FROM mspbot WHERE question LIKE '%" + activity.Text + "%'");
-                /* switch (activity.Text)
+                string holywar_rez = get_holywar_answer("SELECT answer FROM holywars WHERE question LIKE '%" + holywar_text + "%'");
+                if (holywar_rez != "-1")
+                {
+                    texta = holywar_rez;
+                }
+                else
+                {
+                    texta = get_answer("Select answer FROM mspbot WHERE question LIKE '%" + activity.Text + "%'");
+                }
+                    /* switch (activity.Text)
                 {
                     case "кто ты?":
                         texta = "Я, простая симуляция MSP, основанного не реальных людях)";
